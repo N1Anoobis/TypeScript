b2ae0ff2faf2b9e045ab04dd8520f8ec96042d29
N1Anoobis <mrbond84@hotmail.com>
2020-11-21 15:20:29 +0100

    Add sign in template from material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,55 @@
 import React from 'react';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
 function App() {
   return (
     <div className="App">
-     Hack NASA with HTML
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className="paper">
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className="form" noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className="submit"
+          >
+            Sign In
+          </Button>
+        </form>
+      </div>
+    </Container>
     </div>
   );
 }
